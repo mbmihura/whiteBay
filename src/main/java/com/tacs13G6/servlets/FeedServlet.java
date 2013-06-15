b6212290cf6808ecfcce3cebc176598a5d4d5a65
Martin Mihura <mbmihura@gmail.com>
2013-06-15 14:15:31 -0300

    Modifies servlets to return http response codes, test persistence and to work with modified Feed's n' Torrent's models.

--- src/main/java/com/tacs13G6/servlets/FeedServlet.java
+++ src/main/java/com/tacs13G6/servlets/FeedServlet.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
 
@@ -13,11 +16,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+// GAE fecth
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query;
 import com.google.gson.Gson;
 
 //import edu.tacs.group6.models.MockRSSFeedWriter;
 import com.tacs13G6.models.Feed;
 import com.tacs13G6.models.MockFeed;
+import com.tacs13G6.models.exceptions.FeedMalformedException;
 
 /**
  * Servlet implementation class FeedServlet
@@ -53,29 +67,64 @@ public class FeedServlet extends HttpServlet {
                     String feedId = request.getPathInfo().split("/(.*?)",3)[1];
                     Feed feed;
                     //TODO: Buscar feedId en la DB, validar que sea el usuario actual y cargar el objeto en feed.
-                    feed = new MockFeed();
+                    try {
+						feed = new MockFeed();
+						out.println(jsonFactory.toJson(feed));
+					} catch (FeedMalformedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+					}
                     
-                    out.println(jsonFactory.toJson(feed));
+                    
             }
     out.close();  
-    }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            // TODO Auto-generated method stub
     }
 
     /**
      * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
      */
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            PrintWriter out = response.getWriter(); 
+    	String path = request.getPathInfo();
+    	
+    	PrintWriter out = response.getWriter(); 
 
-            String feedId = request.getParameter("feed");
+        String feedId = request.getParameter("feed");
+        
+        out.println("hello Put! " + feedId);
+               
+        Map m = request.getParameterMap();
+        Enumeration e = request.getParameterNames();
+        while (e.hasMoreElements()) {
+        	  Object element = e.nextElement();
+        	  out.println(element);
+        	  out.println(request.getParameter(element.toString()));
+        	}
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        
+        out.println();
+        
+        if (path != null)
+        {
+//        	String feedId = request.getPathInfo().split("/(.*?)",3)[1];
+//            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+//            Key key = KeyFactory.createKey("UserInfo", feedId);
+//            
+//            Entity ent = new Entity("torrent", key);
+//	        ent.setProperty("user", "martin");
+//	        ent.setProperty("date", new Date());
+//	        ent.setProperty("content", "dededededesseffe");
+//
+//	        datastore.put(ent);
             
-            out.print("hello Put! " + feedId);
+        	response.setStatus(HttpServletResponse.SC_CREATED);
+        }
+        else
+        {
+        	response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+        
+        
 
     // Converts JSON string into a collection of Student object.
     //
@@ -93,7 +142,79 @@ public class FeedServlet extends HttpServlet {
      * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
      */
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            // TODO Auto-generated method stub
+    	String path = request.getPathInfo();
+        if (path != null)
+        {
+        	String feedId = request.getPathInfo().split("/(.*?)",3)[1];
+            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+            Key key = KeyFactory.createKey("UserInfo", feedId);
+            try {
+				datastore.get(key);
+				response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+			} catch (EntityNotFoundException e) {
+				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			}
+        }
+        else
+        {
+        	response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest, HttpServletResponse)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String path = request.getPathInfo();
+    	
+    	PrintWriter out = response.getWriter(); 
+
+        String feedId = request.getParameter("feed");
+        
+        out.println("hello Put! " + feedId);
+               
+        Map m = request.getParameterMap();
+        Enumeration e = request.getParameterNames();
+        while (e.hasMoreElements()) {
+        	  Object element = e.nextElement();
+        	  out.println(element);
+        	  out.println(request.getParameter(element.toString()));
+        	}
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        
+        out.println();
+        
+        if (path != null)
+        {
+//        	String feedId = request.getPathInfo().split("/(.*?)",3)[1];
+//            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+//            Key key = KeyFactory.createKey("UserInfo", feedId);
+//            
+//            Entity ent = new Entity("torrent", key);
+//	        ent.setProperty("user", "martin");
+//	        ent.setProperty("date", new Date());
+//	        ent.setProperty("content", "dededededesseffe");
+//
+//	        datastore.put(ent);
+        	response.setStatus(HttpServletResponse.SC_OK);
+            
+        }
+        else
+        {
+        	response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+        
+        
+
+    // Converts JSON string into a collection of Student object.
+    //
+    //Type type = new TypeToken<List<Student>>(){}.getType();
+    //List<Student> studentList = gson.fromJson(jsonStudents, type);
+            //
+    //for (Student student : studentList) {
+    //    System.out.println("student.getName() = " + student.getName());
+    //}
+
+    out.close();
     }
 
 }
--- src/main/java/com/tacs13G6/servlets/RssServlet.java
+++ src/main/java/com/tacs13G6/servlets/RssServlet.java
@@ -10,45 +10,46 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.tacs13G6.models.MockFeed;
 /**
- * Servlet implementation class RssServlet
+ * Permite acceder mediante un pedido GET a los distintos feed del usuario en 
+ * formato XML, el cual puede ser interpretado por un cliente torrent.
  */
 public class RssServlet extends HttpServlet {
-        private static final long serialVersionUID = 1L;
-       
     /**
      * @see HttpServlet#HttpServlet()
      */
     public RssServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-        /**
-         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-         */
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                PrintWriter out = response.getWriter(); 
-                // TODO: definir comportamiento para cuando estran a /rss
-                String feedId = request.getPathInfo().split("/(.*?)",3)[1];
-                try {
-
-                        //TODO: Buscar feedId en la DB
-                        
-                        //Genera el XML y envia el feed. TODO: remplazar por el feed que devuelva la db.
-                        String xml = new MockFeed().toXML();
-                        response.setContentType("text/xml"); 
-                        out.print(xml);
-                } catch (Exception e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                }
-        }
-
-        /**
-         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-         */
-        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                // TODO Auto-generated method stub
-        }
-
+    /**
+     * Metodo GET para obtener a partir de un id, el feed correspondiente en formato XML.
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            PrintWriter out = response.getWriter();
+            String path = request.getPathInfo();
+            if (path != null)
+            {
+	            String feedId = path.split("/(.*?)",3)[1];
+	         // IF (exist in DB) {
+	            //	
+		            try {
+		            	// Se encontro el feed, se genera el XML y envia al cliente.
+		                //TODO: Genera el XML y envia el feed.
+		                String xml = new MockFeed().toXML();
+		                response.setContentType("text/xml"); 
+		                response.setStatus(HttpServletResponse.SC_OK);
+		                out.print(xml);
+		            } catch (Exception e) {
+	                    // No se pudo generar el xml por un error del serializador.
+	                    e.printStackTrace();
+	                    response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		            }
+	            // } else {
+		        //    	response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+	            // }
+            } else {
+            	response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            }
+    }
 }
