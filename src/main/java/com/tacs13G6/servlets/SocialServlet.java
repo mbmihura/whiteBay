db7f5d9129bdbd89d8b270774c8d0557e2068eb3
Martin Mihura <mbmihura@gmail.com>
2013-06-17 00:43:27 -0300

    Implements Feed Persistence: Creates find(), save(), createFeed() methods and updates servlets to make use of them.

--- src/main/java/com/tacs13G6/servlets/SocialServlet.java
+++ src/main/java/com/tacs13G6/servlets/SocialServlet.java
@@ -37,7 +37,7 @@ public class SocialServlet extends HttpServlet {
 		            try {
 		            	// Se encontro el feed, se genera el XML y envia al cliente.
 		                //TODO: Genera el XML y envia el feed.
-		                String xml = new MockFeed().toXML();
+		                String xml = MockFeed.Create().toXML();
 		                response.setContentType("text/xml"); 
 		                response.setStatus(HttpServletResponse.SC_OK);
 		                out.print(xml);
